33bbaa01b09ef7e9a443cfcb5007d9668fce3519
Danapana Akash Reddy <danapanaakashreddy999@gmail.com>
2025-01-17 23:37:51 +0530

    Add files via upload

---
+++ StringPractise.java
@@ -0,0 +1,17 @@
+// public class StringPractise {
+//     public static void main(String[] args){
+//         String str1 = "8";
+//         System.out.println(str1.matches("."));
+//     }
+    
+// }
+
+
+
+
+public class StringPractise{
+    public static void main(String[] args){
+        String str1="abcdef";
+        System.out.println(str1.matches("[a-z]*"));
+    }
+}
