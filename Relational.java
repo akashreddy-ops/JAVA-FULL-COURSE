f2633a34805b73cf0a2fc965c5be38e5a724fe8e
Danapana Akash Reddy <danapanaakashreddy999@gmail.com>
2025-01-25 12:37:05 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,11 @@
+public class Relational {
+    public static void main(String[] args){
+        int n = 5;
+        if (n>0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+}
