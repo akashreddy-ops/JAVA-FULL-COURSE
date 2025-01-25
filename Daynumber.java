b705be64b7f61a730c7951255fdb76d3daabfd40
Danapana Akash Reddy <danapanaakashreddy999@gmail.com>
2025-01-25 12:59:00 +0530

    Add files via upload

---
+++ Daynumber.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Daynumber {
+    public static void main(String[] args){
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Enter a Day Number: ");
+        int day = SC.nextInt();
+        if (day == 1){
+            System.out.println("Monday");
+        }
+        else if (day ==2){
+            System.out.println("Tuesday");
+        }
+        else if (day ==3){
+            System.out.println("Wednesday");
+        }
+        else if (day ==4){
+            System.out.println("Thursday");
+        }
+        else if (day ==5){
+            System.out.println("Friday");
+        }
+        else if (day ==6){
+            System.out.println("Saturday");
+        }
+        else if (day ==7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Invalid Day Number");
+        }
+    }
+    
+}
