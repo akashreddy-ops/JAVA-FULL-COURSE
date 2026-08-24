5d7141ae9ebb69fa9f15770439f43a2015062765
Danapana Akash Reddy <danapanaakashreddy999@gmail.com>
2026-08-24 19:23:06 +0530

    Implement recursive method to print name multiple times

---
+++ N_Times_using_Recursion.java
@@ -0,0 +1,17 @@
+class Solution{
+    public void printName(String name, int count, int n){
+        if(count == n){
+            return;
+        }
+        System.out.println(name);
+        printName(name, count + 1, n);
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int n = 5;
+        String name = "Akash";
+        sol.printName(name, 0, n);
+    }
+}
